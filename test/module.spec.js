95ffae0e3875bd055bbb0d357754b402c68b6c16
Mike Maietta <michaelmaietta@upwork.com>
2021-03-12 18:27:18 -0800

    Adding conditional binding so that we can retain type-safety of the import for things like `permissions.AuthType` as function params with a generic import that is platform-agnostic. Enhancement and fixes: #23 Adding additional test to run on a separate linux node

--- test/module.spec.js
+++ test/module.spec.js
@@ -1,18 +1,19 @@
 const { expect } = require('chai')
-const {
-  askForFoldersAccess,
-  getAuthStatus,
-} = require('../index')
+const permissions = require('../index')
+
+const isMac = process.platform === 'darwin'
+it.ifMac = isMac ? it : it.skip
+it.ifNotMac = isMac ? it.skip : it
 
 describe('node-mac-permissions', () => {
   describe('getAuthStatus()', () => {
     it('should throw on invalid types', () => {
       expect(() => {
-        getAuthStatus('bad-type')
+        permissions.getAuthStatus('bad-type')
       }).to.throw(/bad-type is not a valid type/)
     })
 
-    it('should return a string', () => {
+    it.ifMac('should return a string', () => {
       const types = [
         'contacts',
         'calendar',
@@ -24,12 +25,12 @@ describe('node-mac-permissions', () => {
         'microphone',
         'accessibility',
         'location',
-        'screen'
+        'screen',
       ]
 
       const statuses = ['not determined', 'denied', 'authorized', 'restricted']
       for (const type of types) {
-        const status = getAuthStatus(type)
+        const status = permissions.getAuthStatus(type)
         expect(statuses).to.contain(status)
       }
     })
@@ -38,8 +39,33 @@ describe('node-mac-permissions', () => {
   describe('askForFoldersAccess()', () => {
     it('should throw on invalid types', () => {
       expect(() => {
-        askForFoldersAccess('bad-type')
+        permissions.askForFoldersAccess('bad-type')
       }).to.throw(/bad-type is not a valid protected folder/)
+    })
+  })
+
+  describe('conditional binding', () => {
+    it.ifNotMac('always return undefined for non-mac OS', async () => {
+      const asyncModuleExports = [
+        'askForCalendarAccess',
+        'askForContactsAccess',
+        'askForFullDiskAccess',
+        'askForRemindersAccess',
+        'askForCameraAccess',
+        'askForMicrophoneAccess',
+        'askForPhotosAccess',
+        'askForSpeechRecognitionAccess',
+        'askForScreenCaptureAccess',
+        'askForAccessibilityAccess',
+      ]
+
+      for (const func of asyncModuleExports) {
+        const auth = await permissions[func]()
+        expect(auth).to.be.undefined
+      }
+
+      expect(permissions.getAuthStatus('contacts')).to.be.undefined
+      expect(permissions.askForFoldersAccess('desktop')).to.be.undefined
     })
   })
 })
