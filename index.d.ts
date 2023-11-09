43c43b4b4e5ed4573d2c87167bb1d0622e445a8f
Shashi Shekhar Singh <singh.shashishekhar@gmail.com>
2023-11-10 00:15:58 +0530

    Update type definition of askForScreenCaptureAccess (#68)
    The existing type definition did not have the openPreferences parameter, which exists in the code.

--- index.d.ts
+++ index.d.ts
@@ -12,7 +12,7 @@ export function askForMicrophoneAccess(): Promise<PermissionType>
 export function askForPhotosAccess(accessType?: 'add-only' | 'read-write'): Promise<PermissionType>
 export function askForRemindersAccess(): Promise<Omit<PermissionType, 'restricted'>>
 export function askForSpeechRecognitionAccess(): Promise<Omit<PermissionType, 'restricted'>>
-export function askForScreenCaptureAccess(): undefined
+export function askForScreenCaptureAccess(openPreferences?: boolean): undefined
 export function getAuthStatus(authType: AuthType): PermissionType | 'not determined'
 
 export type AuthType =
